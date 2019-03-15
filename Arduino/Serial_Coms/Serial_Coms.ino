e5c60ea1514efcdcfcd08be4321431f2b8205ee2
bwickenden <32261459+bwickenden@users.noreply.github.com>
2019-03-15 07:00:10 +0000

    Arduino Files
    Serial coms, and LED toggle

---
+++ Arduino/Serial_Coms/Serial_Coms.ino
@@ -0,0 +1,49 @@
+#include <Servo.h>
+const byte ledPin = 13;
+const byte pin = 2;
+volatile byte state = LOW;
+int t = HIGH;
+String content = "";
+
+
+void setup()
+{
+  //Set up PWM pins
+  pinMode(13, OUTPUT); //Test LED
+  
+
+  
+  //Begin serial input
+  Serial.begin(9600); // Baud rate
+  delay(100);
+}
+
+
+void loop() {
+
+  while(Serial.available()) {
+    content = Serial.readString();   
+  }
+
+  if(content.substring(0,5) == "Thumb") { 
+    String Hand =  content.substring(5, (content.length())); 
+   
+    
+      digitalWrite(13, HIGH); // LED ON
+       delay(500);
+      
+      }
+
+    if(content.substring(0,5) == "Index") {  
+    String Hand =  content.substring(5, (content.length())); 
+   
+    
+      digitalWrite(13, LOW); // LED OFF
+       delay(500);
+      
+      }
+    
+      
+      
+  }
+  
