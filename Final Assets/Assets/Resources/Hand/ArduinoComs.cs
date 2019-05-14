c66da641eb158be18bc005384137e58ed1ed98d4
bwickenden <32261459+bwickenden@users.noreply.github.com>
2019-05-14 03:06:51 +0100

    Final Assets for Unity
    Unity Assets folder

---
+++ Final Assets/Assets/Resources/Hand/ArduinoComs.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+using UnityEngine.UI;
+
+public class ArduinoComs : MonoBehaviour
+{
+    [SerializeField]
+    private int portNum;
+
+    SerialPort stream;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Initialise the serial port
+        for (int i = 0; i < 10; i++)
+        {
+            portNum = i;
+            stream = new SerialPort("COM" + portNum, 9600);
+
+            if (!stream.IsOpen)
+            {
+                try
+                {
+                    Debug.Log("Trying to open serial port: " + "COM" + i);
+                    stream.Open();
+                    Debug.Log("Serial Port Open");
+
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Log("Failed to open port. Error: " + ex);
+                }
+            }
+
+
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SendString(string message)
+    {
+        try
+        {
+            stream.Open();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error: " + ex);
+        }
+        //Send message to arduino
+        stream.Write(message);
+        Debug.Log("Message was sent. Message was: " + message);
+        stream.Close();
+    }
+}
